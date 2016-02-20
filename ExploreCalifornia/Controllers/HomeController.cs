ef839b75258d3ee6372cb098b8194dba3ad06c1b
Biri Biri - YY <darkdanito@hotmail.com>
2016-02-21 01:33:00 +0800

    Version 2.6 - Index proof of concept
    - Updated index page with google map API
    - Dropdown list will switch based on selection. Information of the
    located is based on each KML file but not in the DB yet

--- ExploreCalifornia/Controllers/HomeController.cs
+++ ExploreCalifornia/Controllers/HomeController.cs
@@ -8,8 +8,28 @@ namespace ExploreCalifornia.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string Selection)
         {
+            if (!String.IsNullOrEmpty(Selection))
+            {
+                if (Selection == "1")
+                {
+                    ViewBag.modSitesName = "https://www.dropbox.com/s/dxd7uqm8tmztwyg/MUSEUM.kml?dl=1";
+                }
+                else if (Selection == "2")
+                {
+                    ViewBag.modSitesName = "https://www.dropbox.com/s/uqatxl7gxiz9lzl/HISTORICSITES.kml?dl=1";
+                }
+                else if (Selection == "3")
+                {
+                    ViewBag.modSitesName = "https://www.dropbox.com/s/pg72q12826s6et2/MONUMENTS.kml?dl=1";
+                }
+                else
+                {
+                    ViewBag.modSitesName = "https://www.dropbox.com/s/dxd7uqm8tmztwyg/MUSEUM.kml?dl=1";
+                }
+            }
+
             return View();
         }
 
