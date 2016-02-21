2753b055c3aabb42e1cd70a2b14c7bf5f9121200
Biri Biri - YY <darkdanito@hotmail.com>
2016-02-21 20:36:10 +0800

    Bug Fix 5 - Updated Trails Model
    - Added required field to fix the bug of creation of trail with empty
    fields
    - Updated the db

--- ExploreCalifornia/Models/Trail.cs
+++ ExploreCalifornia/Models/Trail.cs
@@ -8,14 +8,18 @@ namespace ExploreCalifornia.Models
     {
         public int Id { get; set; }
 
+        [Required]
         public string Location { get; set; }
 
+        [Required]
         [Display(Name = "Trail Cover")]
         public string TrailCover { get; set; }
 
+        [Required]
         [Display(Name = "Trail Name")]
         public string TrailName { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
     }
