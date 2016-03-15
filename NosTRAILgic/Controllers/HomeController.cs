a66b9d096336f3a1e0c11f254a1256b55d70da09
Biri Biri - YY <darkdanito@hotmail.com>
2016-03-15 20:30:14 +0800

    Version 4.6.1 - Added sample Graph rendering code
    - Updated skeleton graph rendering code

--- NosTRAILgic/Controllers/HomeController.cs
+++ NosTRAILgic/Controllers/HomeController.cs
@@ -1,10 +1,21 @@
 ﻿using System;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
+using NosTRAILgic.DAL;
+using NosTRAILgic.Models;
+using System.Web.Helpers;
 
 namespace NosTRAILgic.Controllers
 {
     public class HomeController : Controller
     {
+        private NosTRAILgicContext db = new NosTRAILgicContext();
+
+        JoinTrail jointrail = new JoinTrail();
+
         public ActionResult Index(string Selection)
         {
             if (!String.IsNullOrEmpty(Selection))
@@ -34,5 +45,60 @@ namespace NosTRAILgic.Controllers
         {            
             return View();
         }
+
+        public ActionResult necrodiver()
+        {
+            //    var LINQTrailParticipantsQuery = from p in db.JoinTrails where p.TrailMeetupID == 1 select p.UserID;
+        //    var LINQTrailParticipantsQuery = from p in db.JoinTrails select p.TrailMeetupID ;
+
+            var orders = (from c in db.JoinTrails
+                          group c by c.TrailMeetupID into g
+                          select new { TrailMeetup = g.Key, Number = g.Count() });
+
+
+
+            var bytes = new Chart(width: 600, height: 400)
+                       .AddTitle("Orders")
+                       .DataBindTable(dataSource: orders, xField: "PaymentTypeID")
+                       .GetBytes("png");
+
+            return File(bytes, "image/bytes");
+        }
+
+        public ActionResult CreateBar()
+        {
+            //Create bar chart
+            var chart = new Chart(width: 300, height: 200)
+            .AddSeries(chartType: "bar",
+                            xValue: new[] { "10 ", "50", "30 ", "70" },
+                            yValues: new[] { "50", "70", "90", "110" })
+                            .GetBytes("png");
+            return File(chart, "image/bytes");
+        }
+
+        public ActionResult CreatePie()
+        {
+            //Create bar chart
+            var chart = new Chart(width: 300, height: 200)
+            .AddSeries(chartType: "pie",
+                            xValue: new[] { "10 ", "50", "30 ", "70" },
+                            yValues: new[] { "50", "70", "90", "110" })
+                            .GetBytes("png");
+            return File(chart, "image/bytes");
+        }
+
+        public ActionResult CreateLine()
+        {
+            //Create bar chart
+            var chart = new Chart(width: 600, height: 200)
+            .AddSeries(chartType: "line",
+                            xValue: new[] { "10 ", "50", "30 ", "70" },
+                            yValues: new[] { "50", "70", "90", "110" })
+                            .GetBytes("png");
+            return File(chart, "image/bytes");
+        }
+
+
+
     }
 }
