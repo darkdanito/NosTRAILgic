10a40cb00f8a3d14ab4c7affa1c72f024daa454a
Biri Biri - YY <darkdanito@hotmail.com>
2016-03-24 11:21:20 +0800

    Version 5.2.3 - Added HomeMapper
    - Code Cleanup
    - Added HomeMapper

--- NosTRAILgic/Controllers/HomeController.cs
+++ NosTRAILgic/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using NosTRAILgic.DAL;
-using NosTRAILgic.Models;
-using System.Web.Helpers;
 
 namespace NosTRAILgic.Controllers
 {
@@ -14,7 +10,7 @@ namespace NosTRAILgic.Controllers
     {
         private NosTRAILgicContext db = new NosTRAILgicContext();
 
-        JoinTrail jointrail = new JoinTrail();
+        HomeMapper homeMapper = new HomeMapper();                  // Data Mapper
 
         public ActionResult Index(string Selection)
         {
@@ -42,74 +38,13 @@ namespace NosTRAILgic.Controllers
             return View();
         }
 
-        public ActionResult Stats()
-        {
-            return View();
-        }
 
         public ActionResult GetLocation(string term)
         {
-            var result = from r in db.Locations
-                         where r.Name.ToLower().StartsWith(term)
-                         select r.Name;
+            var result = homeMapper.getSearchAutoComplete(term);
 
             return Json(result, JsonRequestBehavior.AllowGet);
         }
-
-        public ActionResult necrodiver()
-        {
-            //    var LINQTrailParticipantsQuery = from p in db.JoinTrails where p.TrailMeetupID == 1 select p.UserID;
-            //    var LINQTrailParticipantsQuery = from p in db.JoinTrails select p.TrailMeetupID ;
-
-            var orders = (from c in db.JoinTrails
-                          group c by c.TrailMeetupID into g
-                          select new { TrailMeetup = g.Key, Number = g.Count() });
-
-
-
-            var bytes = new Chart(width: 600, height: 400)
-                       .AddTitle("Orders")
-                       .DataBindTable(dataSource: orders, xField: "PaymentTypeID")
-                       .GetBytes("png");
-
-            return File(bytes, "image/bytes");
-        }
-
-        public ActionResult CreateBar()
-        {
-            //Create bar chart
-            var chart = new Chart(width: 300, height: 200)
-            .AddSeries(chartType: "bar",
-                            xValue: new[] { "10 ", "50", "30 ", "70" },
-                            yValues: new[] { "50", "70", "90", "110" })
-                            .GetBytes("png");
-            return File(chart, "image/bytes");
-        }
-
-        public ActionResult CreatePie()
-        {
-            //Create bar chart
-            var chart = new Chart(width: 300, height: 200)
-            .AddSeries(chartType: "pie",
-                            xValue: new[] { "10 ", "50", "30 ", "70" },
-                            yValues: new[] { "50", "70", "90", "110" })
-                            .GetBytes("png");
-            return File(chart, "image/bytes");
-        }
-
-        public ActionResult CreateLine()
-        {
-            //Create bar chart
-            var chart = new Chart(width: 600, height: 200)
-            .AddSeries(chartType: "line",
-                            xValue: new[] { "18 ", "19", "20 ", "21" },
-                            yValues: new[] { "50", "60", "70", "75" })
-                            .GetBytes("png");
-
-            return File(chart, "image/bytes");
-        }
-
-
 
     }
 }
---
+++ NosTRAILgic/DAL/HomeMapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NosTRAILgic.DAL
+{
+    public class HomeMapper
+    {
+        private NosTRAILgicContext db = new NosTRAILgicContext();
+
+        public IQueryable<string> getSearchAutoComplete(string term)
+        {
+            var result = from r in db.Locations
+                         where r.Name.ToLower().StartsWith(term)
+                         select r.Name;
+
+            return result;
+        }
+    }
+}
