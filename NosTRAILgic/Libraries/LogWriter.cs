fff9632250ce75ae4051fa0c8c91dc7d30805807
Elson Ho <elson_ho@hotmail.com>
2016-04-02 11:41:15 +0800

    Place in LogWriter Class
    Throws in Logwriter class first, implement later

---
+++ NosTRAILgic/Libraries/LogWriter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace NosTRAILgic.Libraries
+{
+    public sealed class LogWriter
+    {
+        private static volatile LogWriter instance;
+        private static object syncRoot = new Object();
+
+        private LogWriter() { }
+
+        public static LogWriter Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new LogWriter();
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        public void LogInfo(string message)
+        {
+            string sPath = AppDomain.CurrentDomain.BaseDirectory + @"\Logs";
+            System.IO.Directory.CreateDirectory(sPath);
+            DateTime currentdt = DateTime.Now;
+            string date = currentdt.ToString("yyyy-MM-dd");
+            TextWriter tw = new StreamWriter(sPath + @"\" + date + ".txt", true);
+            tw.WriteLine("[" + currentdt.ToString() + "] " + message);
+            tw.Close();
+        }
+    }
+}
