0190137a208d5cbc965c10a85ba802382c4ebd89
Biri Biri - YY <darkdanito@hotmail.com>
2016-04-03 15:31:31 +0800

    Version 6.0.1 - Added the missing file

--- NosTRAILgic/Controllers/GeneralController.cs
+++ NosTRAILgic/Controllers/GeneralController.cs
@@ -3,44 +3,20 @@ using System.Web.Mvc;
 
 namespace NosTRAILgic.Controllers
 {
+    /************************************************************************************
+     * Description: This is the General controller for NosTRAILgic                      *
+     *                                                                                  *
+     * Developer: Yun Yong                                                              *
+     *                                                                                  *
+     * Date: 17/02/2016                                                                 *
+     ************************************************************************************/
     public class GeneralController<T> : Controller where T :class
     {
         internal IDataGateway<T> dataGateway;
-
-        //private NosTRAILgicContext db = new NosTRAILgicContext();
 
         //virtual public ActionResult Index(int? id)
         //{
         //    return View(dataGateway.SelectAll());
         //}
-
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    T obj = dataGateway.SelectById(id);
-        //    if (obj == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(obj);
-        //}
-
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    T obj = dataGateway.SelectById(id);
-        //    if (obj == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(obj);
-        //}
-
     }
 }
